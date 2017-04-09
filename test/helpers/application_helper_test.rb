1dc969e59e3b65b92711194ea241d346e763cb9c
ahakopyan <thakhnvac@gmail.com>
2017-04-10 00:18:14 +0300

    Finish layout and routes

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,7 @@
+#require 'test_helper'
+
+#  class ApplicationHelperTest < ActionView::TestCase
+#    assert_equal full_title,         FILL_IN
+#    assert_equal full_title("Help"), FILL_IN
+#  end
+#end
